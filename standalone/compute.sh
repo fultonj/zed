fae2d6a95e22b07737dfef62ed75938eb8f1fb25
John Fulton <fulton@redhat.com>
2022-07-24 17:14:38 +0000

    standalone compute absolute path

--- standalone/compute.sh
+++ standalone/compute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INV=tripleo-ansible/tripleo_ansible/inventory
-PLAY=tripleo-ansible/tripleo_ansible/playbooks/deploy-overcloud-compute.yaml 
+INV=/home/stack/tripleo-ansible/tripleo_ansible/inventory
+PLAY=/home/stack/tripleo-ansible/tripleo_ansible/playbooks/deploy-overcloud-compute.yaml
 
 cp ansible.cfg /home/stack/ansible.cfg
 sudo cp ansible.cfg /root/ansible.cfg
