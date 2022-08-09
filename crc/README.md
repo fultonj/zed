f64fb7cec702ebe1e236c3c62c7a347eeef5ae18
John Fulton <fulton@redhat.com>
2022-08-09 18:49:49 +0000

    add rabbit to crc

--- crc/README.md
+++ crc/README.md
@@ -1,4 +1,4 @@
 # OpenStack Operators on CRC
 
-- crc.sh: clone [install_yamls](https://github.com/openstack-k8s-operators/install_yamls) and install CRC
-
+- [crc.sh](crc.sh): clone [install_yamls](https://github.com/openstack-k8s-operators/install_yamls) and install CRC
+- [rabbit.sh](rabbit.sh): Deploy RabbitMQ
---
+++ crc/rabbit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ ! -d ~/install_yamls ]]; then
+    echo "~/install_yamls missing (did you run crc.sh?)"
+    exit 1
+fi
+
+eval $(crc oc-env)
+oc login -u developer https://api.crc.testing:6443
+
+pushd ~/install_yamls
+
+curl https://raw.githubusercontent.com/openstack-k8s-operators/install_yamls/eae7c79c296fc06301ed141bc1c338cf3056564b/rabbit.yaml > rabbit.yaml
+curl https://raw.githubusercontent.com/openstack-k8s-operators/install_yamls/eae7c79c296fc06301ed141bc1c338cf3056564b/Makefile > Makefile
+git status
+
+sed \
+    -e s/50M/1000M/g \
+    -i rabbit.yaml
+
+make rabbitmq
+sleep 60
+oc apply -f rabbit.yaml
+
+popd
