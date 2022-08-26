ddb531bf313d4f032fceb3216fe3eaf92537abac
John Fulton <fulton@redhat.com>
2022-08-26 17:53:09 +0000

    Switch from genereate-99-custom.py to tripleo-standalone-vars
    In [1] genereate-99-custom.py has been replaced by
    tripleo-standalone-vars. The former output an INI
    inventory and the later outputs a YAML inventory.
    
    Update export.py to copy the new script to the
    standalone controller, run it on the standalone
    controller, and then copy the output back.
    
    Update missing_vars to contain leading whitespace
    so it can contanenate with the output of the new
    script and still produce valid yaml.
    
    Update unapplied-vars.yml to extract the missing
    variables from YAML not INI.
    
    Add a .gitignore to ignore the 99-standalone-vars file
    which is generated by tripleo-standalone-vars.
    
    [1] https://review.opendev.org/c/openstack/tripleo-ansible/+/840509

--- standalone/export.sh
+++ standalone/export.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 CONTROLLER_IP=192.168.24.2
-SRC=/var/lib/config-data/puppet-generated/nova/etc/nova/nova.conf
+SRC=/home/stack/ext/tripleo-ansible/scripts/tripleo-standalone-vars
 DST=/home/stack/ext/tripleo-ansible/tripleo_ansible/inventory/99-custom
 OPT='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
-ssh $OPT $CONTROLLER_IP -l stack \
-    "sudo cp $SRC /tmp/nova.conf; sudo chown stack:stack /tmp/nova.conf"
-scp $OPT stack@$CONTROLLER_IP:/tmp/nova.conf .
-python3 genereate-99-custom.py ./nova.conf
-rm -fv ./nova.conf
-cat 99-custom missing_vars > $DST
+scp $OPT $SRC stack@$CONTROLLER_IP:/home/stack/tripleo-standalone-vars
+
+ssh $OPT $CONTROLLER_IP -l stack "python3 \
+  tripleo-standalone-vars \
+  -c \$(ls /home/stack/ | grep standalone-ansible) \
+  -r Standalone"
+
+scp $OPT stack@$CONTROLLER_IP:/home/stack/99-standalone-vars 99-standalone-vars
+
+if [[ ! -e 99-standalone-vars ]]; then
+    echo "Unable to get a copy of 99-standalone-vars from $CONTROLLER_IP"
+    exit 1
+fi
+cat 99-standalone-vars missing_vars > $DST
