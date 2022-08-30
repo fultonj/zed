1c80646922827a87622770eb170c486cfc9f8239
John Fulton <fulton@redhat.com>
2022-08-30 22:30:22 +0000

    use volumeBindingMode: Immediate

--- crc/crc.sh
+++ crc/crc.sh
@@ -28,22 +28,23 @@ oc whoami
 # https://github.com/openstack-k8s-operators/install_yamls/pull/23
 
 # 1. Testing 'volumeBindingMode: Immediate'
-## curl https://raw.githubusercontent.com/fmount/install_yamls/85e4c5e55c313afdce104e3d53bdb22345154305/crc/storage.yaml > ~/install_yamls/crc/storage.yaml
+curl https://raw.githubusercontent.com/fmount/install_yamls/85e4c5e55c313afdce104e3d53bdb22345154305/crc/storage.yaml > ~/install_yamls/crc/storage.yaml
 
 # 2. Testing 'volumeBindingMode: WaitForFirstConsumer'
-curl https://raw.githubusercontent.com/openstack-k8s-operators/install_yamls/b608987836055560dc51c772196e44c165f11aab/crc/storage.yaml > ~/install_yamls/crc/storage.yaml
+## curl https://raw.githubusercontent.com/openstack-k8s-operators/install_yamls/b608987836055560dc51c772196e44c165f11aab/crc/storage.yaml > ~/install_yamls/crc/storage.yaml
 
 make crc_storage
 # -------------------------------------------------------
-
 # Notes from the clean up:
 #   oc get pv | grep local
-#   for i in $(seq 2 6); do oc delete pv $i; done
-#   for i in $(seq 2 6); do oc delete pv local-storage$i; done
+#   oc get pvc
+#   for i in $(seq 2 6); do oc delete pv local-storage00$i; done
 #   NODE=$(oc get nodes | grep master | awk {'print $1'})
 #   oc debug node/$NODE
 #   chroot /host
 #   ls /mnt/openstack/
-
+#   exit; exit;
+#   make crc_storage_cleanup
+# -------------------------------------------------------
 popd # out of install_yamls
 popd # out of ~
