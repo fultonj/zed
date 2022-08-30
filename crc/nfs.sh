1f0ce7e7a70c0a82b5b6bda6fee556e1b832aa91
John Fulton <fulton@redhat.com>
2022-08-30 19:45:46 +0000

    crc: Change NFS IP and allow v3

--- crc/nfs.sh
+++ crc/nfs.sh
@@ -12,7 +12,7 @@ echo 'Domain in /etc/idmapd.conf is:'
 sudo crudini --set /etc/idmapd.conf General Domain $(hostname)
 crudini --get /etc/idmapd.conf General Domain
 
-echo '/home/nfsshare 10.0.0.0/24(rw,no_root_squash)' > /tmp/exports
+echo '/home/nfsshare 192.168.130.0/24(rw,no_root_squash)' > /tmp/exports
 sudo mv /tmp/exports /etc/exports
 ls -l /etc/exports
 echo 'Contains: '
@@ -20,11 +20,7 @@ cat /etc/exports
 
 sudo mkdir -p /home/nfsshare
 sudo systemctl enable --now rpcbind nfs-server
+sudo systemctl restart nfs-server
 
 # Firewall
-sudo firewall-cmd --add-service=nfs
-
-# Next line is only if we're serving NFS3
-#sudo firewall-cmd --add-service={nfs3,mountd,rpc-bind}
-
-#sudo firewall-cmd --runtime-to-permanent
+sudo firewall-cmd --add-service={nfs,nfs3,mountd,rpc-bind} --zone=libvirt
