89aba823ec89bac0f27d9d514e224ab910894901
John Fulton <fulton@redhat.com>
2022-09-21 18:09:43 +0000

    standalone compute_nova.sh should only set up and call compute.sh

--- standalone/ceph_nova.sh
+++ standalone/ceph_nova.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-
-# SETUP
-if [[ ! -e nova_libvirt_init_secret.sh ]]; then
-    curl https://opendev.org/openstack/tripleo-heat-templates/raw/branch/master/container_config_scripts/nova_libvirt_init_secret.sh -o nova_libvirt_init_secret.sh
-fi
-
-# we want to kolla to runs this for us in our nova containers
-# bash nova_libvirt_init_secret.sh ceph:openstack
-
-INV=/home/stack/ext/tripleo-ansible/tripleo_ansible/inventory
-PLAY=/home/stack/zed/standalone/ceph_nova_playbook.yml
-
-cp ansible.cfg /home/stack/ansible.cfg
-sudo cp ansible.cfg /root/ansible.cfg
-
+# Copy in my changes before running compute.sh
 # -------------------------------------------------------
-# My changes
+# FILES
 
 DST=~/ext/tripleo-ansible/tripleo_ansible/roles/tripleo_nova_libvirt
 pushd ~/tripleo-ansible/tripleo_ansible/roles/tripleo_nova_libvirt/
 cp -v tasks/configure.yml $DST/tasks/configure.yml
+cp -v files/nova_libvirt_init_secret.yaml $DST/files/nova_libvirt_init_secret.yaml
+cp -v tasks/run.yml $DST/tasks/run.yml
+cp -v files/nova_libvirt_init_secret.sh $DST/files/nova_libvirt_init_secret.sh
+cp -v templates/nova_libvirt_init_secret.yaml.j2 $DST/templates/nova_libvirt_init_secret.yaml.j2
 popd
 
+# DST=~/ext/tripleo-ansible/tripleo_ansible/roles/tripleo_container_standalone/
+# pushd ~/tripleo-ansible/tripleo_ansible/roles/tripleo_container_standalone/
+# cp -v tasks/main.yml $DST/tasks/main.yml
+# popd
 # -------------------------------------------------------
 # VARS
 
+INV=/home/stack/ext/tripleo-ansible/tripleo_ansible/inventory
 FSID=$(grep fsid /home/stack/ceph_client.yaml | awk {'print $2'})
 CEPHX=$(grep key /home/stack/ceph_client.yaml | grep -v keys | awk {'print $2'})
 
@@ -33,15 +28,12 @@ cat <<EOF > 09-ceph
 Compute
 
 [tripleo_nova_libvirt:vars]
-tripleo_cinder_enable_rbd_backend=True
-tripleo_ceph_cluster_name=ceph
+tripleo_cinder_enable_rbd_backend=true
 tripleo_ceph_cluster_fsid=$FSID
 tripleo_ceph_client_key=$CEPHX
+tripleo_nova_libvirt_ceph_config_path=/etc/ceph
 EOF
 cp -f 09-ceph $INV/
 
 # -------------------------------------------------------
-# RUN
-pushd /home/stack/
-sudo ansible-playbook -i $INV $PLAY -v
-popd
+bash compute.sh
