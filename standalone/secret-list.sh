93f5ecc19ac06c552d7040191517d70d625402fb
John Fulton <fulton@redhat.com>
2022-09-30 21:27:41 +0000

    have secret-list.sh show secret

--- standalone/secret-list.sh
+++ standalone/secret-list.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CMD="virsh secret-list"
 for POD in $(sudo podman ps --format "{{.Names}}" | grep nova); do
-    echo $POD; sudo podman exec $POD $CMD;
+    echo $POD
+    sudo podman exec $POD virsh secret-list
+    UUID=$(sudo podman exec $POD virsh secret-list | grep ceph | awk {'print $1'})
+    if [[ $(echo $UUID | wc -c) -gt 1 ]]; then
+	sudo podman exec $POD virsh secret-get-value $UUID
+    fi
 done
