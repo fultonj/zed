8b6836e0d455c6472eb4b300653d55b9f63f5020
John Fulton <fulton@redhat.com>
2022-10-10 14:20:53 +0000

    add LS_ETC_CEPH to secret-list.sh

--- standalone/secret-list.sh
+++ standalone/secret-list.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-for POD in $(sudo podman ps --format "{{.Names}}" | grep nova); do
+LS_ETC_CEPH=0
+
+if [ $LS_ETC_CEPH -eq 1 ]; then
+    for POD in $(sudo podman ps --format "{{.Names}}" | grep nova); do
+        echo $POD
+        sudo podman exec $POD ls -l /etc/ceph/
+    done
+fi
+
+for POD in $(sudo podman ps --format "{{.Names}}" | grep nova | grep virtsecretd); do
     echo $POD
     sudo podman exec $POD virsh secret-list
-    UUID=$(sudo podman exec $POD virsh secret-list | grep ceph | awk {'print $1'})
-    if [[ $(echo $UUID | wc -c) -gt 1 ]]; then
-	sudo podman exec $POD virsh secret-get-value $UUID
-    fi
+    for UUID in $(sudo podman exec $POD virsh secret-list | grep ceph | awk {'print $1'}); do
+        if [[ $(echo $UUID | wc -c) -gt 1 ]]; then
+	    sudo podman exec $POD virsh secret-get-value $UUID
+        fi
+    done
 done
