ff55e7528b3b75459ccf80b0ada4aebda781794b
John Fulton <fulton@redhat.com>
2022-10-20 17:19:56 +0000

    Add MAKE, CEPH, and DELETE options to crc/glance.sh
    If CEPH, cr/ceph_cr.sh adds Ceph config to the Glance CR
    If MAKE, run 'make glance' + 'make glance_deploy' from install_yamls
    If DELETE, delete the current Glance CR
    
    These options are handy to update an existing deployment to
    switch glance to a ceph backend.
    
    Also, fix formatting bug in bash conditional and don't 'oc apply'
    without kustomize.

--- crc/glance.sh
+++ crc/glance.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+MAKE=1
+CEPH=0
+DELETE=0
+
 if [[ ! -d ~/install_yamls ]]; then
     echo "~/install_yamls missing (did you run crc.sh?)"
     exit 1
@@ -8,46 +12,49 @@ fi
 eval $(crc oc-env)
 oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
 
-pushd ~/install_yamls
+if [[ $MAKE -eq 1 ]]; then
+    pushd ~/install_yamls
+    # deploy glance operator with make
+    make glance
+    sleep 60
+    # have glance operator deploy glance with make
+    make glance_deploy
+    popd
+fi
 
-# deploy glance operator
-make glance
-
-sleep 60
-make glance_deploy
 
 # modify glance to use use 1G PVs created by crc_storage
 if [[ -e ~/install_yamls/out/openstack/glance/cr/glance_v1beta1_glanceapi.yaml ]]; then
     GLANCE_CR=~/install_yamls/out/openstack/glance/cr/glance_v1beta1_glanceapi.yaml
 fi
-if [[ -e ~/install_yamls/out/openstack/glance/cr/glance_v1beta1_glance.yaml]]; then
+if [[ -e ~/install_yamls/out/openstack/glance/cr/glance_v1beta1_glance.yaml ]]; then
     GLANCE_CR=~/install_yamls/out/openstack/glance/cr/glance_v1beta1_glance.yaml
 fi
 
-# To use external ceph, add the following to a copy of the $GLANCE_CR
-# cephBackend:
-#   cephFsid: "<fsid>"
-#   cephMons: "<ip1>,<ip2>,<ip3>"
-#   cephClientKey: "<key>"
-#   cephUser: "<user>"
-#   cephPools:
-#     glance:
-#       name: "<pool>"
-
 if [[ -e $GLANCE_CR ]]; then
-    sed -i $GLANCE_CR -e s/10G/1G/g
-    echo '  storageClass: local-storage' >> $GLANCE_CR
-    oc apply -f $GLANCE_CR
+    # sed -i $GLANCE_CR -e s/10G/1G/g
+    # echo '  storageClass: local-storage' >> $GLANCE_CR
+    if [[ $DELETE -eq 1 ]]; then
+        echo "Deleting current Glance CR first"
+        oc delete -f $GLANCE_CR
+    fi
+    if [[ $CEPH -eq 1 ]]; then
+        # add Ceph to CR
+        pushd cr
+        CR=$(bash ceph_cr.sh glance)
+        echo "Applying a CR with the following diff:"
+        diff -u $GLANCE_CR $CR
+        cp $CR $GLANCE_CR
+        popd
+    fi
+    oc kustomize ~/install_yamls/out/openstack/glance/cr | oc apply -f -
 else
-    echo "WARNING: $GLANCE_CR does not exist yet. So apply it later."
-    echo "oc apply -f $GLANCE_CR"
+    echo "WARNING: $GLANCE_CR does not exist"
 fi
 
 # OP=$(oc get pods -l control-plane=controller-manager -o name  | grep glance)
 # oc describe $OP
 # oc logs $OP
-
-popd
 
 #oc get pods -l service=glance-default
 #oc get pods -l service=glance-external
