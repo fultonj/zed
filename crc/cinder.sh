00685bfc42ca4e82087dbc613235062ca3de3c3e
John Fulton <fulton@redhat.com>
2022-10-21 18:54:55 +0000

    crc add transport_url to cinder cr

--- crc/cinder.sh
+++ crc/cinder.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MAKE=1
+BACKUP=1
+
 if [[ ! -d ~/install_yamls ]]; then
     echo "~/install_yamls missing (did you run crc.sh?)"
     exit 1
@@ -15,20 +18,27 @@ if [[ $RABBITS -eq 0 ]]; then
     exit 1
 fi
 
-pushd ~/install_yamls
-make cinder_prep
-make cinder
-sleep 60
-make cinder_deploy
-popd
+if [[ $MAKE -eq 1 ]]; then
+    pushd ~/install_yamls
+    make cinder_prep
+    make cinder
+    sleep 60
+    make cinder_deploy
+    popd
+fi
 
 CINDER_CR=~/install_yamls/out/openstack/cinder/cr/cinder_v1beta1_cinder.yaml
 
-echo "Updating Cinder CR to use cephBackend"
+echo "Updating Cinder CR to use cephBackend and setting password for rabbit transport URL"
 pushd cr
-echo "Backing up $CINDER_CR"
-cp -v $CINDER_CR $(basename $CINDER_CR).bak
+if [[ $BACKUP -eq 1 ]]; then
+    echo "Backing up $CINDER_CR"
+    cp -v $CINDER_CR $(basename $CINDER_CR).bak
+fi
 CR=$(bash ceph_cr.sh cinder)
+# xargs trims a string as a side effect
+IP=$(bash rabbit_user_ip.sh | xargs)
+sed -i $CR -e "s/rabbitmq.openstack.svc/$IP/"
 oc delete -f $CINDER_CR
 cp $CR $CINDER_CR
 sleep 10
---
+++ crc/cr/rabbit_user_ip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Add USERNAME w/ PASSWORD to rabbitmq service and output rabbit IP
+#
+# Workaround not yet having the following:
+# https://github.com/openstack-k8s-operators/openstack-operator/pull/27
+# https://github.com/openstack-k8s-operators/cinder-operator/pull/62
+
+USERNAME="openstack"
+PASSWORD="redhat"
+RESETPASSWD=1
+POD=$(oc get pods | grep default-security | grep Running | awk {'print $1'})
+
+function rabbitmqctl {
+    if [[ -z $2 ]]; then
+        oc exec -ti $POD 2> /dev/null -- rabbitmqctl $1
+    elif [[ -z $3 ]]; then
+        oc exec -ti $POD 2> /dev/null -- rabbitmqctl $1 $2
+    else
+        oc exec -ti $POD 2> /dev/null -- rabbitmqctl $1 $2 $3
+    fi
+}
+
+if [[ $(rabbitmqctl list_users | awk {'print $1'} | grep $USERNAME | wc -l) -eq 0 ]]; then
+    #echo "Need to make $USERNAME"
+    rabbitmqctl add_user $USERNAME $PASSWORD
+fi
+if [[ $(rabbitmqctl list_users | awk {'print $1'} | grep $USERNAME | wc -l) -eq 0 ]]; then
+    #echo "Need to make $USERNAME an admin"
+    rabbitmqctl set_user_tags $USERNAME administrator
+fi
+if [[ $RESETPASSWD -eq 1 ]]; then
+    #echo "Ensure username:password for rabbitmqctl is $USERNAME:$PASSWORD"
+    rabbitmqctl change_password $USERNAME $PASSWORD > /dev/null
+fi
+
+oc exec -ti $POD 2> /dev/null -- hostname -I
