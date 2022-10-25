fe1aef8a364fdf32a7ac6e40d7d629c102b7883d
John Fulton <fulton@redhat.com>
2022-10-25 21:28:49 +0000

    script to create ceph-client-conf secret

---
+++ crc/cr/ceph_secret.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OUT=cephSecret.yaml
+SEC=~/cephBackend
+TYPE=glance
+
+declare -A cephBackend
+for KEY in cephFsid cephMons cephClientKey cephUser; do
+    cephBackend[$KEY]=$(grep $KEY ~/cephBackend | awk {'print $2'} | sed s/\"//g)
+done;
+cephBackend[pool]=$(grep $TYPE ~/cephBackend -A 1 \
+                        | tail -1 | awk {'print $2'} | sed s/\"//g)
+
+cat <<EOF > $OUT
+--- 
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ceph-client-conf
+  namespace: openstack
+stringData:
+  ceph.client.${cephBackend[cephUser]}.keyring: |
+    [client.${cephBackend[cephUser]}]
+        key = ${cephBackend[cephClientKey]}
+        caps mgr = "allow *"
+        caps mon = "profile rbd"
+        caps osd = "profile rbd pool=${cephBackend[pool]}"
+  ceph.conf: |
+    [global]
+    fsid = ${cephBackend[cephFsid]}
+    mon_host = ${cephBackend[cephMons]}
+EOF
+
+#oc create -f cephSecret.yaml
+#oc get secret ceph-client-conf -o json | jq -r '.data."ceph.conf"' | base64 -d
