c634e843e885d1817b2102179bc20456c63a93f3
John Fulton <fulton@redhat.com>
2023-02-17 16:08:24 -0500

    show node before role

--- inheritance/fix_host_in_role.sh
+++ inheritance/fix_host_in_role.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+NODE=1
 ROLE=1
-NODE=0
 CLEAN=1
 
 pushd /home/fultonj/zed/inheritance
 
 eval $(crc oc-env)
 oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
+
+if [[ $NODE -gt 0 ]]; then
+    oc create -f node3_from.yaml
+    echo "Showing node"
+    echo "------------"
+    oc get OpenStackDataPlaneNode openstackdataplanenode-sample-3-from-inheritance -o yaml
+    echo ""
+    echo "We can see the hostName and ansibleHost above."
+    echo ""
+    oc get OpenStackDataPlaneNode openstackdataplanenode-sample-3-from-inheritance -o yaml \
+        | grep -i host
+    echo ""
+fi
 
 if [[ $ROLE -gt 0 ]]; then
     oc create -f role.yaml
@@ -22,26 +35,13 @@ if [[ $ROLE -gt 0 ]]; then
     echo ""
 fi
 
-if [[ $NODE -gt 0 ]]; then
-    oc create -f node3_from.yaml
-    echo "Showing node"
-    echo "------------"
-    oc get OpenStackDataPlaneNode openstackdataplanenode-sample-3-from-inheritance -o yaml
-    echo ""
-    echo "We can see the hostName and ansibleHost above."
-    echo ""
-    oc get OpenStackDataPlaneNode openstackdataplanenode-sample-3-from-inheritance -o yaml \
-        | grep -i host
-    echo ""    
-fi
-
 if [[ $CLEAN -gt 0 ]]; then
+    if [[ $NODE -gt 0 ]]; then
+        oc delete -f node3_from.yaml
+    fi
     if [[ $ROLE -gt 0 ]]; then
         echo "Deleting role"
         oc delete -f role.yaml
-    fi
-    if [[ $NODE -gt 0 ]]; then
-        oc delete -f node3_from.yaml
     fi
 fi
 
