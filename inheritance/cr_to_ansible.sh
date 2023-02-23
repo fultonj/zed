701d1ad690db6bd5c2c4f218446b97014b763619
John Fulton <fulton@redhat.com>
2023-02-23 17:06:07 -0500

    Have cr_to_ansible.sh get logs pods in state error

--- inheritance/cr_to_ansible.sh
+++ inheritance/cr_to_ansible.sh
@@ -3,8 +3,9 @@
 
 CREATE=1
 INV=1
-LOGS=0
+LOGS=1
 CLEAN=1
+KILLPODS=0
 
 # This example shows how to create a OpenStackDataPlaneNode
 # CR and observe Ansible configuring the network. The
@@ -45,10 +46,10 @@ eval $(crc oc-env)
 oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
 
 IP=$( sudo virsh -q domifaddr edpm-compute-0 | awk 'NF>1{print $NF}' | cut -d/ -f1 )
-# ssh -i ~/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa root@$IP "uname"
+ssh -i ~/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa root@$IP "uname"
 
 # Ensure the CR has been updated with the correct IP
-if [[ ! $(grep $IP network-edpm-compute-0.yaml | wc -l) -eq 1 ]]; then
+if [[ ! $(grep $IP network-edpm-compute-0.yaml | wc -l) -gt 1 ]]; then
     echo "$IP not in network-edpm-compute-0.yaml"
     exit 1
 fi
@@ -68,17 +69,21 @@ fi
 
 if [ $LOGS -eq 1 ]; then
     echo -e "\nWaiting for Ansible Logs"
-    while [[ $(oc get pods | grep dataplane-deployment | grep Running | wc -l) -eq 0 ]]; do
+    while [[ $(oc get pods | grep dataplane-deployment | egrep "Error" | wc -l) -eq 0 ]]; do
         echo -n "."
         sleep 0.5
     done
     echo ""
-    oc logs $(oc get pods | grep dataplane-deployment | grep Running | awk {'print $1'} | tail -1)
+    oc logs $(oc get pods | grep dataplane-deployment | egrep "Error" | awk {'print $1'} | tail -1)
 fi
 
 if [ $CLEAN -eq 1 ]; then
     echo -e "\nCleaning\n"
     oc delete -f network-edpm-compute-0.yaml
-    oc get configmap -o name | grep dataplanenode | xargs oc delete 
+    oc get configmap -o name | grep dataplanenode | xargs oc delete
+fi
+
+if [ $KILLPODS  -eq 1 ]; then
+    # kill pods left over from previous run?
     oc get pods -o name | grep dataplane-deployment | xargs oc delete
 fi
