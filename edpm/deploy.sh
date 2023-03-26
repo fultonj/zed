e8fbc85cf1f33e304e85803494558070aca0d712
John Fulton <fulton@redhat.com>
2023-03-26 15:30:42 +0000

    edpm/deploy.sh is reading for testing

--- edpm/deploy.sh
+++ edpm/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ATTACH=0
+ATTACH=1
 EDPM_NODE=0
+FORCE_IPS=0
 DEPS=0
 OPER=0
 CONTROL=0
@@ -11,8 +12,7 @@ EDPM_DEPLOY=0
 
 # 0 and 1
 NODES=1
-WAIT=50
-CONTROL_PODS=17
+CONTROL_PODS=16
 
 
 # Run after you have deployed CRC with something like this:
@@ -44,6 +44,11 @@ if [ $EDPM_NODE -eq 1 ]; then
     popd
 fi
 
+if [ $FORCE_IPS -eq 1 ]; then
+    # update this script so handle more than 2 nodes
+    bash ~/zed/edpm/force_ips.sh
+fi
+
 cd ..
 
 if [ $DEPS -eq 1 ]; then
@@ -59,33 +64,60 @@ if [ $CONTROL -eq 1 ]; then
     oc get pods | grep controller
     echo -e "\n\nThere should be $CONTROL_PODS Running OpenStack operator pods above."
     echo -e "(This script will wait indefinitely for all $CONTROL_PODS of them)"
-    while [[ $(oc get pods | grep controller | grep Running | wc -l) -ne $CONTROL_PODS ]];
+    while [[ $(oc get pods | grep controller | grep Running | wc -l) -lt $CONTROL_PODS ]];
     do
         echo -n .
         sleep 1
     done
+    # unset OPENSTACK_CTLPLANE
+    # change repo or branch from explicit defaults as needed
     OPENSTACK_REPO=https://github.com/openstack-k8s-operators/openstack-operator.git \
         OPENSTACK_BRANCH=master \
         make openstack_deploy
 fi
 
 if [ $MARIA -eq 1 ]; then
-    # switch to a wait?
-    if [[ $(oc get pods -o name | grep maria | grep -v controller | wc -l) -eq 1 ]]; then
-        oc exec -it pod/mariadb-openstack -- mysql -uroot -p12345678 -e "set global max_connections = 4000;"
-        oc exec -it  pod/mariadb-openstack -- mysql -uroot -p12345678 -e "show variables like \"max_connections\";"
-    else
-        echo "pod/mariadb-openstack is not running so unable to increase max_connections"
-    fi
+    oc get pods  | grep maria | grep -v controller
+    echo -e "\n\nThere should be at least one Running MariaDB pod above."
+    echo -e "(This script will wait indefinitely for it and then increase max connections)"
+    while [[ $(oc get pods | grep maria | grep -v controller | grep Running | wc -l) -lt 1 ]]; do
+        echo -n .
+        sleep 1
+    done
+    oc exec -it pod/mariadb-openstack -- mysql -uroot -p12345678 -e "set global max_connections = 4000;"
+    oc exec -it  pod/mariadb-openstack -- mysql -uroot -p12345678 -e "show variables like \"max_connections\";"
 fi
 
-# SCHED
-# wait for nova
-#oc get pods | grep nova | grep scheduler
-#oc delete pod -l service=nova-scheduler
+if [ $SCHED -eq 1 ]; then
+    # we need to restart nova-scheduler to pick up cell1, this is a known issue
+    oc get pods | grep nova | grep scheduler
+    echo -e "\n\nThere should be at least one Running nova-scheduler pod above."
+    echo -e "(This script will wait indefinitely for it and then have it pick up cell1)"
+    while [[ $(oc get pods | grep nova | grep scheduler | grep Running | wc -l) -lt 1 ]]; do
+        echo -n .
+        sleep 1
+    done
+    echo "Deleting pods for service nova-scheduler so they will pick up cell1 when they restart"
+    oc delete pod -l service=nova-scheduler
+    sleep 1
+    oc get pods | grep nova | grep scheduler
+    echo "Waiting 5 seconds before checking again if it's running"
+    sleep 5
+    oc get pods | grep nova | grep scheduler
+fi
 
-# EDPM_DEPLOY
-#oc get pods --no-headers=true | egrep -v "Running|Completed" | wc -l # should be 0
-#DATAPLANE_SINGLE_NODE=false make edpm_deploy
+if [ $EDPM_DEPLOY -eq 1 ]; then
+    echo "Looking for pods which are not Running or Completed"
+    oc get pods --no-headers=true | egrep -v "Running|Completed" # should be 0
+    echo -e "\n\nThere should be zero pods listed above"
+    echo -e "(This script will wait indefinitely to reach 0 before EDPM_DEPLOY)"
+    while [[ $(oc get pods --no-headers=true | egrep -v "Running|Completed" |  wc -l) -ne 0 ]]; do
+        echo -n .
+        sleep 1
+    done
+    DATAPLANE_SINGLE_NODE=false make edpm_deploy
+fi
+
+echo -e "\n\nDone. Use watch_ansible.sh or test.sh\n\n"
 
 popd
