6b9adee269150a09e11237e9200ced6d752ca8b1
John Fulton <fulton@redhat.com>
2023-03-26 21:04:03 +0000

    re-write force_ips.sh

--- ng/force_ips.sh
+++ ng/force_ips.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-SSH="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa -l root"
+export LIBVIRT_DEFAULT_URI=qemu:///system
+OPT="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+RSA="~/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa"
+SSH="ssh $OPT -i $RSA -l root"
 
-IP0=$( sudo virsh -q domifaddr edpm-compute-0 | awk 'NF>1{print $NF}' | cut -d/ -f1 )
-if [[ $IP0 != "192.168.122.100" ]]; then
-    if [[ $($SSH $IP0 "ip a | grep 192.168.122.100" | wc -l) -eq 0 ]]; then
-        $SSH $IP0 "sudo ip addr add 192.168.122.100/24 dev eth0"
-    else
-        echo "edpm-compute-0 already has the right IP"
-    fi
-    $SSH 192.168.122.100 "echo good"
-else
-    echo "edpm-compute-0 already has $IP0"
-fi
+NODES=1
 
-IP1=$( sudo virsh -q domifaddr edpm-compute-1 | awk 'NF>1{print $NF}' | cut -d/ -f1 )
-if [[ $IP1 != "192.168.122.101" ]]; then
-    if [[ $($SSH $IP1 "ip a | grep 192.168.122.101" | wc -l) -eq 0 ]]; then
-        $SSH $IP1 "sudo ip addr add 192.168.122.101/24 dev eth0"
+for I in $(seq 0 $NODES); do
+    WANT_IP="192.168.122.10${I}"
+    DOM_IP=$(virsh -q domifaddr edpm-compute-$I \
+                 | awk 'NF>1{print $NF}' | cut -d/ -f1)
+    if [[ -z $DOM_IP ]]; then
+        echo "DOM_IP is empty"
+        ping -c 1 $WANT_IP > /dev/null
+        if [[ $? -eq 0 ]]; then
+            echo -n "$WANT_IP responds to ping and returns the hostname: "
+            $SSH $WANT_IP "hostname"
+        fi
     else
-        echo "edpm-compute-1 already has the the right IP"
+        ping -c 1 $DOM_IP > /dev/null
+        if [[ $? -gt 0 ]]; then
+            echo "DOM_IP $IP does not respond to ping"
+        else
+            if [[ $DOM_IP == $WANT_IP ]]; then
+                echo "edpm-compute-$I already has $WANT_IP"
+            else
+                echo "Going to force IP of edpm-compute-$I to $WANT_IP"
+                if [[ $($SSH $DOM_IP "ip a | grep $WANT_IP" | wc -l) -eq 0 ]]; then
+                    $SSH $DOM_IP "sudo ip addr add $WANT_IP/24 dev eth0"
+                fi
+                echo -n "SSH'ing to $WANT_IP returning the hostname: "
+                $SSH $WANT_IP "hostname"
+            fi
+        fi
     fi
-    $SSH 192.168.122.101 "echo good"
-else
-    echo "edpm-compute-1 already has $IP1"
-fi
+done
