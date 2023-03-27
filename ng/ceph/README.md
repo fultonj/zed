de3bd6babd0450d99185a32622cf30bf9c0081ee
John Fulton <fulton@redhat.com>
2023-03-27 17:29:48 +0000

    add ceph secret script

--- ng/ceph/README.md
+++ ng/ceph/README.md
@@ -1,5 +1,6 @@
 # Ceph on NG
 
-This subdirectory of [ng](../ng) contains variations to use with Ceph.
+This subdirectory of [ng](..) contains variations to use with Ceph.
 
 - Follow [install_ceph](install_ceph.md) to install Ceph on EDPM nodes
+- [ceph_secret.sh](ceph_secret.sh) creates a secret viewable via `oc get secret ceph-conf-files`
---
+++ ng/ceph/ceph_secret.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SSH=$(bash ../ssh_node.sh 0)
+KEY=$($SSH "cat /etc/ceph/ceph.client.openstack.keyring | base64 -w 0")
+CONF=$($SSH "cat /etc/ceph/ceph.conf | base64 -w 0")
+
+cat <<EOF > ceph_secret.yaml
+apiVersion: v1
+data:
+  ceph.client.openstack.keyring: $KEY
+  ceph.conf: $CONF
+kind: Secret
+metadata:
+  name: ceph-conf-files
+  namespace: openstack
+type: Opaque
+EOF
+
+eval $(crc oc-env)
+oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
+
+oc create -f ceph_secret.yaml
