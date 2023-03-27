ef68792c2841bb049118368d68f696afb07fe336
John Fulton <fulton@redhat.com>
2023-03-27 11:16:28 +0000

    add extra output to force_ips.sh

--- ng/force_ips.sh
+++ ng/force_ips.sh
@@ -13,7 +13,7 @@ for I in $(seq 0 $NODES); do
     DOM_IP=$(virsh -q domifaddr edpm-compute-$I \
                  | awk 'NF>1{print $NF}' | cut -d/ -f1)
     if [[ -z $DOM_IP ]]; then
-        echo "DOM_IP is empty"
+        echo "DOM_IP for edpm-compute-$I is empty"
         ping -c 1 $WANT_IP > /dev/null
         if [[ $? -eq 0 ]]; then
             echo -n "$WANT_IP responds to ping and returns the hostname: "
@@ -25,11 +25,16 @@ for I in $(seq 0 $NODES); do
             echo "DOM_IP $IP does not respond to ping"
         else
             if [[ $DOM_IP == $WANT_IP ]]; then
-                echo "edpm-compute-$I already has $WANT_IP"
+                echo "edpm-compute-$I already has $WANT_IP (same as DOM_IP)"
             else
-                echo "Going to force IP of edpm-compute-$I to $WANT_IP"
                 if [[ $($SSH $DOM_IP "ip a | grep $WANT_IP" | wc -l) -eq 0 ]]; then
+                    echo "Going to force IP of edpm-compute-$I to $WANT_IP"
                     $SSH $DOM_IP "sudo ip addr add $WANT_IP/24 dev eth0"
+                else
+                    # In this case the DOM_IP != WANT_IP
+                    # But the WANT_IP was added by above
+                    # and program is being re-run
+                    echo "edpm-compute-$I already has $WANT_IP (diff from DOM_IP)"
                 fi
                 echo -n "SSH'ing to $WANT_IP returning the hostname: "
                 $SSH $WANT_IP "hostname"
