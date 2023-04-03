41294028e891ef98bafd0a73eec372ce7d9a6e6d
John Fulton <fulton@redhat.com>
2023-04-03 16:32:43 -0400

    no need to call 'virsh -q domifaddr'

--- ng/ssh_node.sh
+++ ng/ssh_node.sh
@@ -7,11 +7,7 @@ if [ ! -z "$1" ]; then
     SUFFIX=$1
 fi
 
-IP=$( sudo virsh -q domifaddr edpm-compute-$SUFFIX | awk 'NF>1{print $NF}' | cut -d/ -f1 )
-
-if [[ -z $IP ]]; then
-    IP="192.168.122.10"$SUFFIX
-fi
+IP="192.168.122.10"$SUFFIX
 SSH="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa"
 
 echo "$SSH root@$IP"
