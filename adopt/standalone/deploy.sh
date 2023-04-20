e35fa3cc8d835f33637dfc018d645629fa758723
John Fulton <fulton@redhat.com>
2023-04-20 16:48:10 -0400

    use 122 IP and only verify glance first

--- adopt/standalone/deploy.sh
+++ adopt/standalone/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export INTERFACE=eth0
-export IP=192.168.24.2
-export VIP=192.168.24.3
+export IP=192.168.122.100
+export VIP=192.168.122.108
 export NETMASK=24
 export DNS_SERVERS=192.168.122.1
 export NTP_SERVERS=pool.ntp.org
@@ -38,22 +38,13 @@ if [[ ! -d ~/templates ]]; then
     ln -s /usr/share/openstack-tripleo-heat-templates ~/templates
 fi
 
-# sudo pkill -9 heat-all # Remove any previously running heat processes
 sudo openstack tripleo deploy \
   --templates ~/templates \
   --local-ip=$IP/$NETMASK \
   --control-virtual-ip $VIP \
   -r ~/templates/roles/Standalone.yaml \
   -e ~/templates/environments/standalone/standalone-tripleo.yaml \
-  -e ~/templates/environments/cephadm/cephadm.yaml \
-  -e ~/templates/environments/cephadm/ceph-rgw.yaml \
-  -e cephadm_overrides.yaml \
   -e ~/containers-prepare-parameters.yaml \
   -e standalone_parameters.yaml \
   --output-dir $HOME \
   --standalone $@
-
-#  --keep-running \
-
-#  -e ~/templates/environments/ceph-ansible/ceph-ansible.yaml \
-#  -e ceph_ansible_overrides.yaml \
