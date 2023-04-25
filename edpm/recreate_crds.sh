e509cf7f2f7f46a8bb34ada48344d26966dc08d6
John Fulton <fulton@redhat.com>
2023-04-25 17:53:24 +0000

    Include recreating openstackdataplaneservices in edpm/recreate_crds.sh
    Used to test:
    
    https://github.com/openstack-k8s-operators/dataplane-operator/pull/158

--- edpm/recreate_crds.sh
+++ edpm/recreate_crds.sh
@@ -7,6 +7,7 @@ pushd ~/dataplane-operator
 oc delete crd openstackdataplanenodes.dataplane.openstack.org
 oc delete crd openstackdataplaneroles.dataplane.openstack.org
 oc delete crd openstackdataplanes.dataplane.openstack.org
+oc delete crd openstackdataplaneservices.dataplane.openstack.org
 oc apply -f config/crd/bases/
 popd
 
