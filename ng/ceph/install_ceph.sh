e94b02009d1cf1d55b41b58bcf680763747f4d6b
John Fulton <fulton@redhat.com>
2023-06-08 09:17:00 -0400

    use SSH option with scp and assume network isolation by default

--- ng/ceph/install_ceph.sh
+++ ng/ceph/install_ceph.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 NET=0
-ISO=0
 PRE=0
 BOOT=0
 SINGLE_OSD=0
@@ -13,6 +12,7 @@ NODES=2
 RSA="/home/$USER/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa"
 OPT="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 IP=192.168.122.100
+MON_IP=172.18.0.100
 
 if [ $NET -eq 1 ]; then
     # install os-net-config on edpm-compute-0 to configure network isolation
@@ -23,23 +23,13 @@ if [ $NET -eq 1 ]; then
     ssh -i $RSA $OPT root@$IP "bash /tmp/network.sh"
 fi
 
-if [ $ISO -eq 1 ]; then
-    # If we're deploying with network isolation
-    MON_IP=172.18.0.100
-    crudini --set initial_ceph.conf global cluster_network 172.20.0.0/24
-    crudini --set initial_ceph.conf global public_network 172.18.0.0/24
-    crudini --set initial_ceph.conf mon public_network 172.18.0.0/24
-else
-    MON_IP=$IP
-fi
-
 if [ $PRE -eq 1 ]; then
     URL=https://raw.githubusercontent.com/ceph/ceph/quincy/src/cephadm/cephadm
     for I in $(seq 0 $NODES); do
 	IPL="192.168.122.10${I}"
-	scp -i $RSA hosts root@$IP:/etc/hosts
-	scp -i $RSA ceph_spec.yml root@$IP:/root/ceph_spec.yml
-	scp -i $RSA initial_ceph.conf root@$IP:/root/initial_ceph.conf
+	scp -i $RSA $OPT hosts root@$IP:/etc/hosts
+	scp -i $RSA $OPT ceph_spec.yml root@$IP:/root/ceph_spec.yml
+	scp -i $RSA $OPT initial_ceph.conf root@$IP:/root/initial_ceph.conf
 	ssh -i $RSA $OPT root@$IPL "curl --silent --remote-name --location $URL"
 	ssh -i $RSA $OPT root@$IPL "chmod +x cephadm"
 	ssh -i $RSA $OPT root@$IPL "mkdir -p /etc/ceph"
@@ -57,7 +47,7 @@ if [ $SINGLE_OSD -eq 1 ]; then
 fi
 
 if [ $SSH_KEYS -eq 1 ]; then
-    scp -i $RSA root@$IP:/etc/ceph/ceph.pub .
+    scp -i $RSA $OPT root@$IP:/etc/ceph/ceph.pub .
     URL=$(cat ceph.pub | curl -F 'sprunge=<-' http://sprunge.us)
     rm ceph.pub
 
@@ -66,12 +56,12 @@ if [ $SSH_KEYS -eq 1 ]; then
 fi
 
 if [ $SPEC -eq 1 ]; then
-    scp -i $RSA apply_spec.sh root@$IP:/root/apply_spec.sh
+    scp -i $RSA $OPT apply_spec.sh root@$IP:/root/apply_spec.sh
     $(bash ../ssh_node.sh) "bash /root/apply_spec.sh"
 fi
 
 if [ $CEPHX -eq 1 ]; then
-    scp -i $RSA cephx.sh root@$IP:/root/cephx.sh
+    scp -i $RSA $OPT cephx.sh root@$IP:/root/cephx.sh
     $(bash ../ssh_node.sh) "bash /root/cephx.sh"
     $(bash ../ssh_node.sh) "ls -l /etc/ceph/"
 fi
